1c2b651c983533f36e73f07f86371f87f0cb4ec0
Pragyan Aryal <pragyanaryal4@gmail.com>
2023-11-30 23:16:35 -0500

    Formatted scripts

--- Scripts/fetch_job_description.py
+++ Scripts/fetch_job_description.py
@@ -3,64 +3,65 @@ from DrissionPage import ChromiumPage
 from bs4 import BeautifulSoup
 import pandas as pd
 
-code = 'BA'
-
-records = pd.read_csv(code + '.tsv', sep='\t')
-
-jobKeys = [records.loc[i, "Key"] for i in range(len(records))]
+codes = ['BA', 'DE']
 
 job_url = 'https://www.indeed.com/viewjob?jk={}&from=vjs&viewtype=embedded'
 
 page = ChromiumPage()
 
-descriptions = []
+for code in codes:
+    records = pd.read_csv(code + '.tsv', sep='\t')
 
-for key in jobKeys:
+    jobKeys = [records.loc[i, "Key"] for i in range(len(records))]
 
-    l_ = 'Currently Fetched ' + '{:3.2f}'.format((len(descriptions) / len(jobKeys)) * 100) + '% job description'
-    l_ = l_ + '. Remaining ' + str(len(jobKeys) - len(descriptions)).ljust(4) + ' jobs'
-    l_ = l_ + '. Completed ' + str(len(descriptions)).ljust(4) + ' jobs.'
+    descriptions = []
 
-    print(l_)
+    for key in jobKeys:
 
-    page.get(job_url.format(key))
-    page.wait.load_complete()
+        l_ = 'Currently Fetched ' + '{:3.2f}'.format((len(descriptions) / len(jobKeys)) * 100) + '% job description'
+        l_ = l_ + '. Remaining ' + str(len(jobKeys) - len(descriptions)).ljust(4) + ' jobs'
+        l_ = l_ + '. Completed ' + str(len(descriptions)).ljust(4) + ' jobs.'
 
-    err = False
-    while not err:
+        print(l_)
+
+        page.get(job_url.format(key))
+        page.wait.load_complete()
+
+        err = False
+        while not err:
+            try:
+                page.wait.ele_display('#jobDescriptionText', timeout=20)
+                err = True
+            except Exception:
+                time.sleep(10)
+
+        time.sleep(0.2)
+
+        soup = BeautifulSoup(page.html, 'lxml')
+
         try:
-            page.wait.ele_display('#jobDescriptionText', timeout=20)
-            err = True
+            d = soup.find('div', {'id': 'jobDescriptionText'})
+            g = d.extract()
+            f = [h.text.replace('\n', '') for h in g if h.text != '\n']
+
+            a = ''.join(f)
         except Exception:
-            time.sleep(10)
+            a = 'NA'
 
-    time.sleep(0.2)
+        try:
+            benef = soup.find('div', {'id': 'benefits'})
+            lis = benef.find_all('li')
+            benefits = [ben.text for ben in lis]
+        except Exception:
+            benefits = []
 
-    soup = BeautifulSoup(page.html, 'lxml')
+        descriptions.append({'key': key, 'desc': a, 'benefits': benefits})
 
-    try:
-        d = soup.find('div', {'id': 'jobDescriptionText'})
-        g = d.extract()
-        f = [h.text.replace('\n', '') for h in g if h.text != '\n']
+        time.sleep(0.5)
 
-        a = ''.join(f)
-    except Exception:
-        a = 'NA'
-
-    try:
-        benef = soup.find('div', {'id': 'benefits'})
-        lis = benef.find_all('li')
-        benefits = [ben.text for ben in lis]
-    except Exception:
-        benefits = []
-
-    descriptions.append({'key': key, 'desc': a, 'benefits': benefits})
-
-    time.sleep(0.5)
-
-with open(code + '_desc.tsv', 'a', encoding='utf-8') as outfile:
-    for i in descriptions:
-        outfile.write(i['key'] + '\t' + i['desc'] + '\t' + str(i['benefits']) + '\n')
-outfile.close()
+    with open(code + '_desc.tsv', 'a', encoding='utf-8') as outfile:
+        for i in descriptions:
+            outfile.write(i['key'] + '\t' + i['desc'] + '\t' + str(i['benefits']) + '\n')
+    outfile.close()
 
 page.quit()
