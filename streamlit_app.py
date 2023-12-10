608b16dc2bd9e44598f2462c07b5d4568588b1e7
Pragyan Aryal <pragyanaryal4@gmail.com>
2023-12-09 19:14:13 -0500

    Re-wrote merge location script

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,13 @@
 import streamlit as st
-st.title("CareerCompass")
+import pandas as pd
+import altair as alt
 
-jobs_list = ["Software Engineer", "Data Analyst", "Product Manager", "Project Manager","Business Analyst", "Data Engineer", "Mechanical Engineer", "CAE Engineer", "FEA Engineer", "Civil Engineer"]
-job_title = st.selectbox("Job Title", options = jobs_list)
+import numpy as np
 
-years_ex = st.slider("How many years of experience do you have?", 0, 10)
-if years_ex <= 3:
-   st.write("You are considered as entry level professional in the job market.")
-elif years_ex > 3 and years_ex <=7:
-   st.write("You are considered as mid level professional in the job market.")
-else:
-   st.write("You are considered as senior level professional in the job market.")
-  
-need_h1 = st.checkbox("Do you need sponsorship now or in future?")
-if need_h1:
-   button1 = st.button("Next1")
+st.title("Map")
+
+df = pd.read_parquet('Data/Indeed/merged.parquet', columns=['latitude', 'longitude'])
+
+df = df.dropna()
+
+st.map(df, size=20, color='#0044ff')
