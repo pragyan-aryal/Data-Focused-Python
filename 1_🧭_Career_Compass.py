c3eaf96b4965d349d23d52416546653b07c9d96f
unknown <amoghborikar@gmail.com>
2023-12-09 23:12:02 -0500

    App

--- 1_🧭_Career_Compass.py
+++ 1_🧭_Career_Compass.py
@@ -8,3 +8,11 @@ st.set_page_config(
 st.title("Career Compass")
 
 st.sidebar.success("Select a page above")
+
+st.header('🎯Vision', divider='black')
+
+multi = '''Empower job seekers by providing comprehensive data on job opportunities,
+required skills, salary ranges, visa sponsorship, and location-specific hiring trends,
+allowing users to make informed career decisions.
+'''
+st.markdown(multi)
