8c1771888fc9e857928595c3fd5b1f6a7a0e05b1
unknown <amoghborikar@gmail.com>
2023-12-10 02:35:54 -0500

    App

--- pages/3_📍_Map.py
+++ pages/3_📍_Map.py
@@ -1,15 +1,42 @@
 import streamlit as st
 import pandas as pd
-import altair as alt
+import plotly.express as px
 
-import numpy as np
+df = pd.read_parquet('Data//Indeed/Indeed_Data.parquet')
+df = df.loc[df['country_code'] == 'USA']
+
+category_to_job = {
+    'SE': "Software Engineer",
+    'DA': "Data Analyst",
+    'PM': "Product Manager",
+    'PJM': "Project Manager",
+    'BA': "Business Analyst",
+    'DE': "Data Engineer",
+    'ME': "Mechanical Engineer",
+    'CAE': "CAE Engineer",
+    'FEA': "FEA Engineer",
+    'CE': "Civil Engineer"
+}
 
 st.title("Map")
 
-df = pd.read_parquet(r'Data/Indeed/Indeed_Data.parquet', columns=['latitude', 'longitude', 'country_code'])
+job_to_category = {v: k for k, v in category_to_job.items()}
 
-df = df.dropna()
+# Let the user select a job title
+selected_job = st.selectbox("What is your desired job title?", options=list(category_to_job.values()))
 
-df = df.loc[df['country_code'] == 'USA']
+# Get the related category_code based on the selected job title
+selected_category = job_to_category[selected_job]
 
-st.map(df, size=20, color='#0044ff', zoom=2)
+# Filtering the dataframe based on chosen job title
+filtered_df = df[df['Category'] == selected_category]
+
+
+# filtered_df = filtered_df[['latitude', 'longitude', 'region']][selected_category]
+
+fig = px.density_mapbox(filtered_df, lat = 'latitude', lon = 'longitude',
+                        radius = 8,
+                        # center = dict(lat = 42.83, lon = -8.35),
+                        zoom = 2,
+                        mapbox_style = 'open-street-map')
+st.plotly_chart(fig)
